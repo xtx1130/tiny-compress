221040a7a8baac0567c1f46febdecdae7de803ee
xtx1130 <xtx1130@gmail.com>
2018-02-05 15:49:02 +0800

    change: main class created

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
  *@description 使用tinypng.com接口实现图片压缩
  *@param {Object} opts
  *@param {String} opts.key tinypng.com用户唯一识别码
- **@param {String} opts.inPath 源文件存储地址
- *@param {String} opts.outPath 压缩后的文件存储地址
+ **@param {String} opts.inDir 源文件存储地址
+ *@param {String} opts.outDir 压缩后的文件存储地址
  */
 'use strict'
 
@@ -12,6 +12,7 @@ const path = require('path')
 const klaw = require('klaw')
 
 let setTinyKey = Symbol.for('private#setTinyKey')
+let orginToCompress = []
 
 class Tinier{
   constructor(opts){
@@ -22,7 +23,22 @@ class Tinier{
   }
   setConfig(opts){
     this.key = opts.key || 'dh-9FQbZX3vxYjfpJFX5Do3Oa86QJNBa'
-    this.inPath = opts.inPath || path.join(process.cwd(), './images')
-    this.outPath = opts.outPath || path.join(process.cwd(), './tinyout')
+    this.inDir = opts.inDir || path.join(process.cwd(), './images')
+    this.outDir = opts.outDir || path.join(process.cwd(), './tinyout')
+    return this
+  }
+  main(){
+    return new Promise((resolve, reject) => {
+      klaw(this.inDir).on('data', async item => {
+        let fileName = path.parse(item.path).base
+        if (fileName.match(/jpg|png|jpeg/ig)) {
+          let outPath = path.join(this.outDir, fileName)
+          await tinify.fromFile(item.path).toFile(outPath)
+          orginToCompress.push([item.path, outPath])
+        }
+      })
+      .on('end', () => resolve(orginToCompress))
+      .on('error', (err, item) => reject(err))
+    })
   }
 }
